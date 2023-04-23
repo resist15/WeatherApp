02a8adf30bf8bc20610e1114faeb928551fcb041
resist15 <sourav@project404.xyz>
2023-04-23 17:46:37 +0530

    WeatherApp: Implement OpenWeatherAPI for Weather Data
    - We Might Change API Due to its 20 Requests / Day Limitations
    
    Change-Id: I861f8072ccc9ee04f501f26ee9e8a5f25f725b21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit{
+
+  constructor(private weatherService: WeatherService) {
+
+  }
+  cityName: string = 'Pune';
+  weatherData?: WeatherData;
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string){
+      this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(response);
+        }
+      });
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+
+    XRapidAPIKeyHeaderValue:'bf718fa2admshc63aca9fcd65251p12ea5fjsn792c837f314c',
+
+};
