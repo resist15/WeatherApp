68a59a0f846a4b84e41b79162ad6f98e36c77e0e
resist15 <sourav@project404.xyz>
2023-04-23 17:46:37 +0530

    WeatherApp: Implement OpenWeatherAPI for Weather Data
    - We Might Change API Due to its 20 Requests / Day Limitations
    
    Change-Id: I861f8072ccc9ee04f501f26ee9e8a5f25f725b21

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    weatherApiBaseUrl: 'https://openweather43.p.rapidapi.com/weather',
+    
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+
+    XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+
+    XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+
+    XRapidAPIKeyHeaderValue:'key-here-bhai',
+
+};
